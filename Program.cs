0bb33fb035ea38747fa6db84e83a42e72a0a80d3
Denis Grim <dehorovici@gmail.com>
2021-10-14 00:54:43 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ElectricityBillMSIC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
